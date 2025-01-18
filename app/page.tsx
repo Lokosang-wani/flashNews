52d8d62aafcc9fd9f2751e3a7c2725a67bd61c41
Felix Wani <lokosangwani21@Gmail.com>
2025-01-18 18:04:21 +0200

    added api and json news types files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { ArticleApi } from "./api/article-api";
 
 
-export default function MainPage() {
+export default async function MainPage() {
+  console.log("ENV API KEY accessible on server only", process.env.API_KEY);
+  const articles = await ArticleApi.fetchToday();
   return (
-    <div>
-      <div>Hello from index page</div>
+    <div className="">
+      {JSON.stringify(articles)}
       </div>
   );
 }
